df9891658fbe03a5b1f3fbefe71a5977e408d435
secilkara <secilkara@gmail.com>
2023-12-21 00:21:33 +0300

    initial

---
+++ src/main/java/com/project/couriertracking/CouriertrackingApplication.java
@@ -0,0 +1,13 @@
+package com.project.couriertracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouriertrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouriertrackingApplication.class, args);
+	}
+
+}
